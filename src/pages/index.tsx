973d1fd2109ff8187a20555ac2a6d3a11bcd0c67
Uziano Dev <lucianoperaltadefazio@gmail.com>
2023-05-28 21:35:09 -0300

    Commiting everything from notebook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,24 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [posX, setPosX] = useState(0)
+  const [posY, setPosY] = useState(0)
+ 
+//  unified coordinates
+  const [pos, setPos] = useState({
+    x: 0,
+    y: 0,
+  })
+
+  useEffect(() => {
+    setPosX( posX + 0.01)
+  }, [posX])
+
   return (
     <>
       <Head>
@@ -22,7 +36,26 @@ export default function Home() {
           height={200}
           width={200}
           alt="Profile Pic"
-        /> 
+        />
+        
+        <div 
+          id="circle" 
+          style={{ 
+            backgroundColor: 'red',
+            position: 'absolute',
+            left: posX, 
+            top: posY,
+            width: 100,
+            height: 100,
+            borderRadius: '50%'
+          }}
+        >
+        </div>
+
+        <button onClick={()=>{setPosX( posX + 50)}}>Move +</button>
+        <button onClick={()=>{setPosY( posY - 50)}}>Move +</button>
+        <button onClick={()=>{setPosX( posX - 50)}}>Move -</button>
+        <button onClick={()=>{setPosY( posY + 50)}}>Move -</button>
       </main>
     </>
   );
